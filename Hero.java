606fdaa566ed83d345848c31e977fb4b41fa1783
kamaseinu <48120534+kamaseinu@users.noreply.github.com>
2020-01-08 23:02:37 +0900

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -8,17 +8,20 @@ public class Hero {
 	public int hp = 0;
 	public int max_hp;
 	public int power = 10;
-	public int guard = 3;
-	public int mp = 0;
+	public int guard = 5;
+	public int mp = 10;
 	public int max_mp;
 	public int level = 1;
 	public int exp = 0;
+	public int total_exp;
 	public int money = 0;
 	public int death = 0;
 	public int kill = 0;
-	public ArrayList<String> Magic = new ArrayList<String>();
+	public ArrayList<String> Magic_name = new ArrayList<String>();
+	public ArrayList<Integer> Magic_power = new ArrayList<Integer>();
+	public ArrayList<Integer> Magic_energy = new ArrayList<Integer>();
 	public Object[] item_box;
-	public String weapon;
+	
 	
 	public Hero () {
 		this.name = name;
@@ -30,9 +33,21 @@ public class Hero {
 		this.max_mp = mp;
 		this.level = level;
 		this.exp = exp;
+		this.total_exp = exp;
 		this.money = money;
 		this.death = death;
 		this.kill = kill;
+		Magic_name.add("火の玉");	Magic_power.add(10);	Magic_energy.add(10);
+		Magic_name.add("吹雪");	Magic_power.add(15);	Magic_energy.add(20);
+		Magic_name.add("雷");	Magic_power.add(20);	Magic_energy.add(30);
+		Magic_name.add("台風");	Magic_power.add(30);	Magic_energy.add(40);
+		Magic_name.add("地震");	Magic_power.add(40);	Magic_energy.add(50);
+		Magic_name.add("津波");	Magic_power.add(50);	Magic_energy.add(60);
+		Magic_name.add("隕石");	Magic_power.add(60);	Magic_energy.add(70);
+		Magic_name.add("終焉");	Magic_power.add(10000);	Magic_energy.add(666);
+		this.Magic_name = Magic_name;
+		this.Magic_power = Magic_power;
+		this.Magic_energy = Magic_energy;
 	}
 	
 	//攻撃
@@ -40,7 +55,7 @@ public class Hero {
 		System.out.println(a.name + "は" + b.name + "に攻撃した");
 		int damage = a.power + ran.nextInt(4);
 		int critical_hit = ran.nextInt(20);
-		if (critical_hit == 0) {		//5%nの確率でクリティカルヒット
+		if (critical_hit == 0) {		//5%の確率でクリティカルヒット
 			System.out.println("クリティカルヒット！！！");
 			damage *= 2;
 		}
@@ -55,8 +70,15 @@ public class Hero {
 	}
 	
 	//魔法
-	public void magic () {
-		
+	public void Magic (Hero a, Enemy b, int c) {
+		System.out.println(a.name + "は" + a.Magic_name.get(c) + "を使った!");
+		a.mp -= a.Magic_energy.get(c);
+		System.out.println(b.name + "は" + a.Magic_power.get(c) + "のダメージ!");
+		b.hp -= a.Magic_power.get(c);
+		if (b.hp < 0) {
+			b.hp = 0;
+		}
+		System.out.println("------------------------------------------------");
 	}
 	
 	//眠る(最大体力の3分の1回復)
@@ -77,5 +99,27 @@ public class Hero {
 	public void hide() {
 		
 	}
+	
+	
+	public void level_check(Hero a) {
+		while (a.exp > a.level*50) {
+			if (a.exp >= a.level*50) {
+				System.out.println(a.name + "はレベルが上がった!");
+				System.out.printf("レベル\t%3d --> %3d", a.level, a.level+1);					System.out.println();
+				System.out.printf("最大体力\t%3d --> %3d", a.max_hp, a.max_hp+a.level*10);	System.out.println();
+				a.max_hp += a.level*10;
+				System.out.printf("最大魔力\t%3d --> %3d", a.max_mp, a.max_mp+10);	System.out.println();
+				a.max_mp += 10;
+				System.out.printf("攻撃力\t%3d --> %3d", a.power, a.power+a.level*3);			System.out.println();
+				a.power += a.level*3; 
+				System.out.printf("防御力\t%3d --> %3d", a.guard, a.guard+a.level*3);			System.out.println();
+				a.guard += a.level*3;
+				a.exp -= a.level*50;
+				a.level++;
+			}
+		}
+	}
+	
+	
 	
 }
